695a51a5ec3b7661542f4c7d3549e8ce8447ea77
Alexey Andriyanov <alan@al-an.info>
2015-05-13 14:27:51 +0400

    check_http: refactor, fix bug with ipvs add delay
    epilog() used to have two bool parameters that were not easy to understand.
    
    Also, turning the succeeded real-server into UP state was performed in the
    another http_connect_thread, in vs->delay_loop seconds after successful check.
    'Find eventual url end' code block moved from http_connect_thread function to
    the epilog to fix this bug.

--- keepalived/include/check_http.h
+++ keepalived/include/check_http.h
@@ -93,7 +93,7 @@ typedef struct _http_checker {
 
 /* Define prototypes */
 extern void install_http_check_keyword(void);
-extern int epilog(thread_t *, int, int, int);
+extern int epilog(thread_t *, int);
 extern int timeout_epilog(thread_t *, char *, char *);
 extern url_t *fetch_next_url(http_checker_t *);
 extern int http_process_response(request_t *, int, int);
