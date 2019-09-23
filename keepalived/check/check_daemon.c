b09afc8b0fcb1e7d295663be5f6d8b5443e0b0f8
Aleksandr Balezin <gescheit@yandex-team.ru>
2019-09-23 20:25:05 +0300

    убран неиспользуемые код

--- keepalived/check/check_daemon.c
+++ keepalived/check/check_daemon.c
@@ -53,7 +53,6 @@ extern char *checkers_pidfile;
 static void
 stop_check(void)
 {
-	status_socket_close();
 	/* Destroy master thread */
 	check_masters_stop();
 	signal_handler_destroy();
@@ -148,13 +147,8 @@ start_check(void)
 	link_vsg_to_vs();
 
 	/* Processing differential configuration parsing */
-	if (reload) {
+	if (reload)
 		clear_diff_services();
-		status_socket_reload();
-	} else {
-		status_socket_init();
-	}
-
 
 	/* Initialize IPVS topology */
 	if (!init_services()) {
--- keepalived/check/ipvsstatus.c
+++ keepalived/check/ipvsstatus.c
@@ -3,13 +3,12 @@
 #include "check_data.h"
 
 #include "memory.h"
+#include "utils.h"
 #include "scheduler.h"
 #include "logger.h"
 #include "tcp_server.h"
 #include "ipvsstatus.h"
 
-static int ipvs_status_dumper(thread_t*);
-static thread_t *dumper_thread;
 static pthread_t tcp_server_thread_id = 0;
 
 #define BUF_ALLOC_SIZE 1024
@@ -26,33 +25,6 @@ int start_status_server() {
 		printf("Thread not created.\n");
 	}
 	return ret;
-}
-
-int status_socket_init(void) {
-	dumper_thread = thread_add_timer(master, ipvs_status_dumper, NULL,
-			5 * TIMER_HZ);
-	log_message(LOG_INFO, "registered dumper_thread");
-	if (tcp_server_thread_id == 0)
-		start_status_server();
-	//dump_config(res);
-	return 0;
-}
-
-int status_socket_close(void) {
-	// TODO: close socket
-	log_message(LOG_INFO, "terminating dumper_thread");
-	thread_cancel(dumper_thread);
-	return 0;
-}
-
-int status_socket_reload(void) {
-	if (1 /* TODO: socket config changed */) {
-		log_message(LOG_INFO, "reloading dumper_thread");
-
-		status_socket_close();
-		status_socket_init();
-	}
-	return 0;
 }
 
 int check_increase_buf(int buf_left, int *allocated, char **res) {
@@ -190,17 +162,4 @@ int dump_config(char **out_res, int *size, dump_format format) {
 		log_message(LOG_INFO, "error during dump_config()");
 		FREE(res);
 		return status;
-}
-
-static int ipvs_status_dumper(thread_t *thread) {
-	global_data = alloc_global_data();
-	char **res;
-	log_message(LOG_INFO, "dumper_thread timer event");
-	//dump_check_data()
-	//dump_global_data(global_data);
-
-	//dump_config(res);
-	dumper_thread = thread_add_timer(master, ipvs_status_dumper, NULL,
-			5 * TIMER_HZ);
-	return 0;
 }
